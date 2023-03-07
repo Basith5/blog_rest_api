080ddeaf7f3f7835fe83632d1449062b9ca82d34
ABDUL BASITH <110540511+Basith5@users.noreply.github.com>
2023-03-07 18:23:36 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -20,7 +20,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
-////////////////////////////////////////
+
 //schema for the update body using zod
 const Schema = z.object({
   blogId: z.number(),
@@ -30,7 +30,7 @@ const Schema = z.object({
   richText: z.string().optional(),
   img_micro: z.string().optional(),
 });
-////////////////////////////////////////
+
 //schema for the request body using Zod
 const blogSchema = z.object({
   blogTitle: z.string().min(1),
@@ -39,7 +39,9 @@ const blogSchema = z.object({
   richText: z.string().min(1),
   img_micro: z.string().optional(),
 });
-//////////////////////////////////////////////////////////////////////////////
+
+//adding a new blog..................
+//-------------------------------------------------------------------------------------------------
 app.post('/addblog', upload.single('img_micro'), async (req: Request, res: Response) => {
   try {
     // Validate request body using the schema
@@ -89,30 +91,10 @@ app.post('/addblog', upload.single('img_micro'), async (req: Request, res: Respo
   }
 });
 
-///////////////////////////////////////////////////////////////////////////
-// //adding data to the blog table
-// app.post('/addblog', async (req: Request, res: Response) => {
-//   try {
-//     // Validate request body using the schema
-//     const { blogTitle, shortDescription, blogCategory, richText } = blogSchema.parse(req.body);
 
-//     // Sanitize richText field
-//     const sanitizedRichText = sanitizeHtml(richText);
 
-//     // Insert data into MySQL database
-//     const [result] = await pool.query(
-//       'INSERT INTO blog (blogTitle, shortDescription, blogCategory, richText) VALUES (?, ?, ?, ?)',
-//       [blogTitle, shortDescription, blogCategory, sanitizedRichText]
-//     );
-
-//     res.status(201).json({ id: (result as mysql.OkPacket).insertId, message: 'Blog post created successfully' });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Something went wrong' });
-//   }
-// });
-////////////////////////////////////////////////////////////////////////
 //getting data from the blog table
+//---------------------------------------------------------------
 app.get('/readBlog', async (req: Request, res: Response) => {
   try {
     // Get data from MySQL database
@@ -124,8 +106,10 @@ app.get('/readBlog', async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Something went wrong' });
   }
 });
-///////////////////////////////////////////////////////////////////////////
+
+
 //single blog post by ID from the database
+//---------------------------------------------------------------------
 app.get('/readBlog/:id', async (req, res) => {
   const id = req.params.id;
   try {
@@ -140,32 +124,10 @@ app.get('/readBlog/:id', async (req, res) => {
     res.status(500).send('Error retrieving blog post');
   }
 });
-///////////////////////////////////////////////////////////////////////////
-// //updating data in the blog table
-// app.put('/updateBlog/:id', async (req: Request, res: Response) => {
-//   try {
-//     // Validate request body
-//     blogSchema.parse(req.body);
 
-//     const { blogTitle, shortDescription, blogCategory, richText } = req.body;
-//     const { id } = req.params;
 
-//     // Sanitize richText
-//     const sanitizedText = sanitizeHtml(richText);
-
-//     // Update data in MySQL database
-//     const [result] = await pool.query(
-//       'UPDATE blog SET blogTitle = ?, shortDescription = ?, blogCategory = ?, richText = ? WHERE id = ?',
-//       [blogTitle, shortDescription, blogCategory, sanitizedText, id]
-//     );
-
-//     res.status(200).json({ msg: 'success' });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Something went wrong' });
-//   }
-// });
-///////////////////////////////////////////////////////////////////////////
+//update or editing a blog in single or full field
+//-------------------------------------------------------------------------------
 app.patch('/editBlog/:id', async (req: Request, res: Response) => {
   try {
     let columnToUpdate;
@@ -201,67 +163,10 @@ app.patch('/editBlog/:id', async (req: Request, res: Response) => {
   }
 });
 
-///////////////////////////////////////////////////////////////////////////
-// app.patch('/editBlog/:id', upload.single('img_micro'), async (req: Request, res: Response) => {
-//   try {
-//     const { blogTitle, shortDescription, blogCategory, richText } = Schema.pick({
-//       blogTitle: true,
-//       shortDescription: true,
-//       blogCategory: true,
-//       richText: true,
-//     }).parse(req.body);
 
-//     // Get the filename of the uploaded image and save to database
-//     const img_micro = req.file ? req.file.filename : '';
 
-//     // Update data in MySQL database
-//     const [result] = await pool.query(
-//       'UPDATE blog SET blogTitle=?, shortDescription=?, blogCategory=?, richText=?, img_micro=? WHERE id=?',
-//       [blogTitle, shortDescription, blogCategory, richText, img_micro, req.params.id]
-//     );
-
-//     if ((result as OkPacket).affectedRows === 0) {
-//       res.status(404).json({ error: 'Data not found' });
-//     } else {
-//       res.status(200).json({ msg: 'success' });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Something went wrong' });
-//   }
-// });
-
-///////////////////////////////////////////////////////////////////////////
-// //updating data in the blog table
-// app.patch('/editBlog/:id', async (req: Request, res: Response) => {
-//   try {
-//     const { blogTitle, shortDescription, blogCategory, richText } = Schema.pick({
-//       blogTitle: true,
-//       shortDescription: true,
-//       blogCategory: true,
-//       richText: true,
-//     }).parse(req.body);
-
-//     // // Sanitize richText before updating database
-//     // const sanitizedRichText = sanitizeHtml(richText);
-
-//     const [result] = await pool.query(
-//       'UPDATE blog SET blogTitle=?, shortDescription=?, blogCategory=?, richText=? WHERE id=?',
-//       [blogTitle, shortDescription, blogCategory, richText, req.params.id]
-//     );
-
-//     if ((result as OkPacket).affectedRows === 0) {
-//       res.status(404).json({ error: 'Data not found' });
-//     } else {
-//       res.status(200).json({ msg: 'success' });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Something went wrong' });
-//   }
-// });
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //delete a blog post from the database
+//----------------------------------------------------------------------------
 app.delete('/deleteBlog/:id', async (req: Request, res: Response) => {
   const postId = req.params.id;
   try {
@@ -280,9 +185,10 @@ app.delete('/deleteBlog/:id', async (req: Request, res: Response) => {
   }
 });
 
-///////////////////////////////////////////////////////////////////////////////
+
 
 // Start the server
+//------------------------------------------------------------------------------
 app.listen(5000, () => {
   console.log('Server started on port 5000');
 });
